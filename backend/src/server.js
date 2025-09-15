fb1d0252e24adb234c008a46cf061ff1a5a78c0a
Khushi Agrawal <khushiagrawal10041995@gmail.com>
2025-09-15 23:50:02 +0530

    changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,44 @@
-import express from 'express';
+import express from "express";
+import { ENV } from "../config/env.js";
+import { inngest, functions } from "../config/inngest.js";
+import { serve } from "inngest/express";
+import { connectDB } from "../config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
-import {ENV} from '../config/env.js';
-import { inngest, functions } from '../config/inngest.js';
-import { serve } from 'inngest/express';
+const app = express();
 
-import { connectDB } from '../config/db.js';
-import { clerkMiddleware } from '@clerk/express';
-
-const app= express();
 app.use(clerkMiddleware());
 app.use(express.json());
-// Set up the "/api/inngest" (recommended) routes with the serve handler
+
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Vercel + Express 🚀");
 });
 
-// app.listen(ENV.PORT,()=>{
-//     console.log(`Server is running on port ${ENV.PORT}`);
-//     connectDB();
-// });
 
-// const startServer = async () => {//vercel doesnt use app.listen so we need to connectDB before starting the server//first problem arrived here 
-//     try {
-//       await connectDB();
-//       if(ENV.NODE_ENV !== "production"){
-//       app.listen(ENV.PORT, () => {
-//         console.log(`Server is running on port ${ENV.PORT}`);
-//       });
-//     }
-//     } catch (error) {
-//       console.error('Failed to start server:', error);
-//         process.exit(1);
-//     }
-//   };
-//    startServer();
+if (ENV.NODE_ENV !== "production") {
+  const startServer = async () => {
+    try {
+      await connectDB();
+      app.listen(ENV.PORT, () => {
+        console.log(`Server running at http://localhost:${ENV.PORT}`);
+      });
+    } catch (error) {
+      console.error("Failed to start server:", error);
+      process.exit(1);
+    }
+  };
+
+  startServer();
+} else {
+
+  connectDB().catch((err) => {
+    console.error("Failed to connect DB on Vercel:", err);
+  });
+}
+
+
 export default app;
