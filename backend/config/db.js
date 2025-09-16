a243c3828ac9e828bc80587113dd18a1e0e39caf
Khushi Agrawal <khushiagrawal10041995@gmail.com>
2025-09-16 11:43:11 +0530

    ..

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,20 +5,16 @@ let isConnected = false; // track connection state
 
 export const connectDB = async () => {
   if (isConnected) {
-    // Reuse existing connection
     return;
   }
 
   try {
-    const conn = await mongoose.connect(ENV.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(ENV.MONGO_URI);
 
     isConnected = true;
-    console.log("✅ MongoDB connected:", conn.connection.host);
+    console.log(" MongoDB connected:", conn.connection.host);
   } catch (error) {
-    console.error("❌ Error connecting to MongoDB:", error.message);
-    throw error; // don't kill process, let Vercel handle retry
+    console.error(" Error connecting to MongoDB:", error.message);
+    throw error;
   }
 };
